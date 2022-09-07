b3e489c1ed9d8fa7d2d78ffe53e2066f96156a0d
arjunsingh14 <arjunsingh140601@gmail.com>
2022-09-07 18:50:12 -0400

    Routes now get all users and create a new user

--- server/server.js
+++ server/server.js
@@ -1,18 +1,13 @@
-const express = require('express')
+const express = require("express");
 require("dotenv").config();
 //Sequalize is an object relational mapper (ORM) that converts Postgres to JSON
-const {
-  Sequelize,
-  Model,
-  DataTypes,
-}  = require("sequelize");
-
-
+const { Sequelize, Model, DataTypes } = require("sequelize");
 
 const app = express();
 app.use(express.json());
 
 //connecting to database
+// eslint-disable-next-line no-undef
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialectOptions: {
     ssl: {
@@ -25,9 +20,7 @@ const sequelize = new Sequelize(process.env.DATABASE_URL, {
 //models
 
 
-//implementing types (typescript)
-class User extends Model{}
-  
+class User extends Model {}
 
 //model initializization
 User.init(
@@ -35,28 +28,37 @@ User.init(
     id: {
       type: DataTypes.INTEGER,
       autoIncrement: true,
-      primaryKey: true
+      primaryKey: true,
     },
     username: {
       type: DataTypes.TEXT,
-
     },
     password: {
-      type: DataTypes.TEXT
-    }
+      type: DataTypes.TEXT,
+    },
   },
   {
     sequelize,
     modelName: "user",
+    timestamps: false
   }
 );
 
-
-
-app.get("/api/v1/user", async (_req, res) => {
-  const allUsers = await User.findAll()
+app.get("/api/v1/user", async (req, res) => {
+  const allUsers = await User.findAll();
   res.json({ allUsers });
 });
+
+app.post("/api/v1/user", async (req, res) => {
+  try {
+    console.log(req.body);
+    const user = await User.create(req.body);
+    res.json(user);
+  } catch (error) {
+    console.log(error)
+  }
+ 
+})
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
